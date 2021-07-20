fa85a0abb6fd41d8d1bbfd3bc79c34b0d4ab94b4
Hritik <hritikkhurana10sm@gmail.com>
2021-07-20 16:35:12 +0530

    added views ejs files and render from controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 //this is a callback function , it will return an object so ti receive it we do .home there
 module.exports.home = function home(req , res){
 
-    res.end('<h1>Hey I am in Contoller!!</h1>');
+   // res.end('<h1>Hey I am in Contoller!!</h1>');
+  
+   res.render('home' , {
+        'title' : "Social Clone"
+   })
 }
