7ea39249ef797826d8c929f9a71d919fba4013c2
Hritik <hritikkhurana10sm@gmail.com>
2021-07-26 01:58:18 +0530

    created the signin and signup form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,31 @@ module.exports.profile = function profile(req , res){
         title : "User Profile"
     });
 }
+
+module.exports.signin = function(req , res){
+
+    return res.render('user_sign_in' , {
+
+        title : "Sign In"
+    });
+}
+
+module.exports.signup = function(req , res){
+
+    return res.render('user_sign_up' , {
+
+        title : "Sign Up"
+    });
+}
+
+//get the sign up data
+module.exports.create = function(req , res){
+
+     //todo   
+}
+
+//sign in and create a session
+module.exports.createSession = function(req , res){
+
+  //todo
+}
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ const user_controller = require('../controllers/users_controller');
 
 router.get('/profile' ,user_controller.profile);
 
+router.get('/signin' , user_controller.signin );
+
+router.get('/signup' , user_controller.signup );
+
+
 module.exports = router;
