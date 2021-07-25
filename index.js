d0dc7816f60f5f663806dc2ee80ec24364742231
Hritik <hritikkhurana10sm@gmail.com>
2021-07-25 17:51:51 +0530

    added static files and layout extractStyles and Scripts as true and used <%- style%> and similarly script

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 
+//extract style and scripts from subpages into the layout
+app.set('layout extractStyles' , true);
+app.set('layout extractScripts' , true);
+
+//including assets files
+app.use(express.static('./assets'));
+
 //setting up the view engine
 app.set('view engine' , 'ejs');
 app.set('views' , './views');
