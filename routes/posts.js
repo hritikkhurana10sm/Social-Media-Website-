f21b694dc5c7a88a143042dc28e1cbb40fd5a50a
Hritik <hritikkhurana10sm@gmail.com>
2021-08-08 18:43:01 +0530

    added a authentication in posting : if user is not signed in , he/she not allowed to make posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const passport = require('passport');
 //this is basically used to separate the routes and controller
 
 //to require it
 const posts_controller = require('../controllers/posts_controller');
 
 console.log("post controller");
-router.post('/new' , posts_controller.newUserPosts);
+router.post('/new' , passport.checkAuthentication , posts_controller.newUserPosts);
 
 module.exports = router;
